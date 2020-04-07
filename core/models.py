7de9548f6419a02ada02dcff385c1c5ffb9ceefa
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2020-04-07 23:26:07 +0200

    Einsatz model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Einsatz(models.Model):
+
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Einsatz"
+        verbose_name_plural = "Einsätze"
--- dokumentation/models.py
+++ dokumentation/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+#from django.contrib.auth.models import User
 
 class Meldung(models.Model):
     MELDUNGSRICHTUNG = (
@@ -18,3 +19,14 @@ class Meldung(models.Model):
 
     class Meta:
         verbose_name_plural = "Meldungen"
+
+class Signatur(models.Model):
+
+    zeit = models.DateTimeField(auto_now_add=True, blank=True)
+    #user = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.zeit.strftime("%d%H%M")
+
+    class Meta:
+        verbose_name_plural = "Signaturen"
