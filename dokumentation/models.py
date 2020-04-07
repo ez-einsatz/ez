fc247d7f22fdd2f357938fb0fd219466397b63f5
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2020-04-07 02:26:03 +0200

    first model

--- dokumentation/models.py
+++ dokumentation/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Meldung(models.Model):
+    MELDUNGSRICHTUNG = (
+            (0, 'Eingang'),
+            (1, 'Ausgang'),
+            (2, 'Gesprächsnotiz'),
+        )
+    richtung = models.IntegerField(default=0,
+            choices=MELDUNGSRICHTUNG)
+
+    von = models.CharField(max_length=200)
+    an = models.CharField(max_length=200)
+    inhalt = models.TextField()
+
+    def __str__(self):
+        return self.inhalt
+
+    class Meta:
+        verbose_name_plural = "Meldungen"
--- ez0/settings.py
+++ ez0/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'dokumentation',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de-de'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
