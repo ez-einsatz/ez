0ef3e3d34227137582bdda6da38a3608c4341688
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2020-04-08 02:28:51 +0200

    fix image upload

--- einheiten/models.py
+++ einheiten/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Einheit(models.Model):
 
     name = models.CharField(max_length=200)
-    taktisches_zeichen = models.FileField(upload_to='media/einheiten/taktisches_zeichen/')
+    taktisches_zeichen = models.ImageField(verbose_name="Taktisches Zeichen", upload_to='einheiten/taktisches_zeichen/',blank=True)
 
     def __str__(self):
         return self.name
