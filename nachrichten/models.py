e1d461ee25a2f426b73e160e2da1dba386a784d6
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2020-04-08 02:29:14 +0200

    new nachrichten models

--- nachrichten/models.py
+++ nachrichten/models.py
@@ -14,24 +14,23 @@ class Signatur(models.Model):
         verbose_name_plural = "Signaturen"
 
 class Nachricht(models.Model):
-    MELDUNGSRICHTUNG = (
+    MELDERICHTUNG = (
             (0, 'Eingang'),
             (1, 'Ausgang'),
         )
-    richtung = models.IntegerField(default=0,
-            choices=MELDUNGSRICHTUNG)
+    richtung = models.IntegerField(default=0, choices=MELDERICHTUNG)
 
-    notiz = models.BooleanField(default=False)
+    notiz = models.BooleanField(verbose_name="Gesprächsnotiz", default=False)
 
     absender = models.CharField(max_length=200)
     anschrift = models.CharField(max_length=200)
     inhalt = models.TextField()
 
     VORRANGSTUFEN = (
-            (0, 'eee'),
-            (1, 'sss'),
-            (2, 'bbb'),
-            (3, 'aaa'),
+            (0, 'einfach'), #eee
+            (1, 'sofort'), #sss
+            (2, 'BLITZ'), #bbb
+            #(3, 'STAATSNOT'), #aaa
         )
     vorrangstufe = models.IntegerField(default=0, choices=VORRANGSTUFEN)
 
@@ -44,10 +43,10 @@ class Nachricht(models.Model):
             (5, 'Fax'),
             (6, 'MIS'),
         )
-    aufnahmeweg = models.IntegerField(default=0, choices=MELDEWEGE)
+    aufnahmeweg = models.IntegerField(default=0, blank=True, null=True, choices=MELDEWEGE)
     aufnahmevermerk = models.ForeignKey(Signatur, blank=True, null=True, on_delete=models.CASCADE,related_name="aufnahmevermerke",related_query_name="aufnahmevermerk")
     annahmevermerk = models.ForeignKey(Signatur, blank=True, null=True, on_delete=models.CASCADE,related_name="annahmevermerke",related_query_name="annahmevermerk")
-    befoerderungsweg = models.IntegerField(default=0, choices=MELDEWEGE)
+    befoerderungsweg = models.IntegerField(default=0, blank=True, null=True, choices=MELDEWEGE)
     befoerderungsvermerk = models.ForeignKey(Signatur, verbose_name="Beförderungsvermerk", blank=True, null=True, on_delete=models.CASCADE,related_name="befoerderungsvermerke",related_query_name="befoerderungsvermerk")
 
     def __str__(self):
